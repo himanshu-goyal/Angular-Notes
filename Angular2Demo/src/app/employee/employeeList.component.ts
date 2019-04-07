6ac9bafb6f0a887e66cdd8594119a895e5632022
himanshu <himanshu@DESKTOP-P687APJ>
2019-04-07 17:19:58 -0600

    Example of nested and contanier component.

--- Angular2Demo/src/app/employee/employeeList.component.ts
+++ Angular2Demo/src/app/employee/employeeList.component.ts
@@ -13,20 +13,21 @@ export class EmployeeListComponent {
             { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
             { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
             { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '11/16/1980' },
+            { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 6700.826, dateOfBirth: '10/15/1980' },
         ];
     }
 
-    getEmployees(): void {
-        this.employees = [
-            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '6/25/1988' },
-            { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
-            { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
-            { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '11/16/1980' },
-            { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 6700.826, dateOfBirth: '10/15/1980' },
-        ]
-    }
+    //getEmployees(): void {
+    //    this.employees = [
+    //        { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '6/25/1988' },
+    //        { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
+    //        { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
+    //        { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '11/16/1980' },
+    //        { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 6700.826, dateOfBirth: '10/15/1980' },
+    //    ]
+    //}
 
-    trackByEmpCode(index: number, employee: any): string{
-        return employee.code;
-    } 
+    //trackByEmpCode(index: number, employee: any): string{
+    //    return employee.code;
+    //} 
 }
