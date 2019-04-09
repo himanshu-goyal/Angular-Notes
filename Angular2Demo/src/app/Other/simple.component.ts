149278f0aa84f295d092d6b156770c3f8465b6eb
himanshu <himanshu@DESKTOP-P687APJ>
2019-04-09 00:24:20 -0600

    simple component

---
+++ Angular2Demo/src/app/Other/simple.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'simple',
+    template: `You Entered: {{simpleInput}}`
+})
+
+export class SimpleComponent {
+   @Input() simpleInput: string;
+}
